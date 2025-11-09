081eb6e598dcd45fc33617bd5419de853d5c58d2
velarno <git@arnov.dev>
2025-11-09 08:06:55 +0100

    chore: added sha to brew formula

--- homebrew/Formula/figgit.rb
+++ homebrew/Formula/figgit.rb
@@ -7,16 +7,16 @@ class Figgit < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/velarno/figgit/releases/download/v#{version}/figgit-aarch64-apple-darwin.tar.gz"
-      sha256 "REPLACE_WITH_ACTUAL_SHA256"
+      sha256 "sha256:1760c49f71ecabb94a1fce4977bfa84b1b05f41380fb0b71df51c26b96068d6f"
     else
       url "https://github.com/velarno/figgit/releases/download/v#{version}/figgit-x86_64-apple-darwin.tar.gz"
-      sha256 "REPLACE_WITH_ACTUAL_SHA256"
+      sha256 "sha256:d21de75cda8c32c0aba64ccc48fc1a2fe3536cafeefffd7362c40d9db451eecd"
     end
   end
 
   on_linux do
     url "https://github.com/velarno/figgit/releases/download/v#{version}/figgit-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "REPLACE_WITH_ACTUAL_SHA256"
+    sha256 "sha256:87d621d562c72af573e7e1f41ef3f02515fb31d322d652acafc4ee7e460a1c8b"
   end
 
   def install
